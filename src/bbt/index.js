dd51874215b94c6126faeb6c7c25d4c05dc85bfb
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-11-13 00:52:17 +0100

    cleaner bbt error handling

--- src/bbt/index.js
+++ src/bbt/index.js
@@ -2,12 +2,9 @@ import { parse as parseBibtex } from '@retorquere/bibtex-parser'
 
 export function parse (text) {
   return parseBibtex(text, {
-    errorHandler (message) {
-      if (/Unhandled (command|{\\r .})/.test(message)) {
-        // skip some non-fatal errors
-      } else {
-        throw new SyntaxError(message)
-      }
+    errorHandler (err) {
+      if (err.name === 'TeXError') return // ignore unhandled TeX constructs
+      throw err
     }
   })
 }
